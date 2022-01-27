de26d5cdb532397082be47ed475b847acd22c3f8
mi-mran <mi.mran@hotmail.com>
2022-01-28 05:59:31 +0800

    Add MSE and RMSE functions

--- machine_learning/utils.py
+++ machine_learning/utils.py
@@ -4,3 +4,13 @@ def accuracy(y_true, y_pred):
     accuracy = np.sum(y_true == y_pred) / len(y_true)
 
     return accuracy
+
+def mse(y_true, y_pred):
+    mse = np.mean(np.square(y_true - y_pred))
+
+    return mse
+
+def rmse(y_true, y_pred):
+    rmse = np.sqrt(mse(y_true, y_pred))
+
+    return rmse
