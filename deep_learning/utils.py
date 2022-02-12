c196af5b5c4d81fb94595563a75c75ba4ca8eb72
mi-mran <mi.mran@hotmail.com>
2022-02-13 03:14:27 +0800

    Add forward function

--- deep_learning/utils.py
+++ deep_learning/utils.py
@@ -2,7 +2,13 @@ import numpy as np
 
 class Dense:
     def __init__(self, n_inputs, n_neurons):
+        # number of rows corresponds to number of inputs
+        # number of columns corresponds to number of neurons
         self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
+
+        # since we only need one bias per neuron, we create a 1xn_neurons array
         self.biases = np.zeros((1, n_neurons))
 
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
         
